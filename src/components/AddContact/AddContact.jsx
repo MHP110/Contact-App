7bbe05cb899aef2381bd91505ea4fc45f3ed6444
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-01-07 14:50:44 +0330

    add 'AddContact' component

---
+++ src/components/AddContact/AddContact.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+const AddContact = ({ onSubmit }) => {
+	const [contact, setContact] = useState({ name: '', email: '' });
+
+	const changeHandler = (e) => {
+		setContact({ ...contact, [e.target.name]: e.target.value });
+	};
+
+	return (
+		<form className='flex flex-col justify-center items-center'>
+			<section className='flex justify-center items-center'>
+				<label>name</label>
+				<input
+					type='text'
+					name='name'
+					value={contact.name}
+					onChange={changeHandler}
+					className='bg-slate-50 border-2 border-violet-500 rounded-md px-2 py-1 my-1 ml-2 outline-none'
+				/>
+			</section>
+
+			<section className='flex justify-center items-center'>
+				<label>email</label>
+				<input
+					type='text'
+					name='email'
+					value={contact.email}
+					onChange={changeHandler}
+					className='bg-slate-50 border-2 border-violet-500 rounded-md px-2 py-1 my-1 ml-2 outline-none'
+				/>
+			</section>
+
+			<button
+				type='submit'
+				onClick={(e) => onSubmit(e)}
+				className='bg-violet-500 text-white px-3 py-1 rounded-md'>
+				Add Contact
+			</button>
+		</form>
+	);
+};
+
+export default AddContact;
