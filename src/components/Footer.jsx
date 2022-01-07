fca712a43c91e712f1c46926ca598ce4c5c8e0b3
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-01-07 13:44:23 +0330

    add Header and Footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return ( <footer className="bg-violet-100">
+
+    </footer> );
+}
+ 
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return ( <header className=''>
+        <span>CONTACT APP</span>
+    </header> );
+}
+ 
+export default Header;
